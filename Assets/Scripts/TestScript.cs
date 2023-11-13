d7f7fb8627a07928edc4ef45c1753cf88105463b
qin-andrew123 <qin.andrew123@gmail.com>
2023-11-13 11:26:27 -0800

    small changes attempted to add the core package buf failed

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class TestScript : RenderPipelineAsset
+{
+    protected override RenderPipeline CreatePipeline()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
